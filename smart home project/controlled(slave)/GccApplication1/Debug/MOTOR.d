25d80b0f2317ff02a03f361cc932fc99d32a9d20
Mustafa Muhammed <MustafaMuhammed2103@gmail.com>
2021-10-30 01:23:02 +0200

    Add files via upload

---
+++ smart home project/controlled(slave)/GccApplication1/Debug/MOTOR.d
@@ -0,0 +1,19 @@
+MOTOR.d MOTOR.o: .././MOTOR.c .././MOTOR.h .././MOTOR_CONFIG.h .././DIO.h \
+ .././ATMEGA32_REGISTERS.h .././standard_types.h .././TIMER0.h \
+ .././TIMER0_CONFIG.h .././common_macros.h
+
+.././MOTOR.h:
+
+.././MOTOR_CONFIG.h:
+
+.././DIO.h:
+
+.././ATMEGA32_REGISTERS.h:
+
+.././standard_types.h:
+
+.././TIMER0.h:
+
+.././TIMER0_CONFIG.h:
+
+.././common_macros.h:
